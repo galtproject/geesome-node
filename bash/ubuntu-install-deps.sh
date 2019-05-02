8efa2b49f83a00456229f3b8bd77bb73471cbaa6
jonybang <Jonybange@gmail.com>
2019-05-03 01:25:02 +0200

    add multipart

--- bash/ubuntu-install-deps.sh
+++ bash/ubuntu-install-deps.sh
@@ -1,4 +1,6 @@
 sudo apt-get update
-sudo apt-get install build-essential libssl-dev
+sudo apt-get install python build-essential libssl-dev mysql-server
+mysql -uroot -proot -e "create database geesome_core;"
+mysql -uroot -proot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-nvm install 10.15 && nvm use 10.15 && nvm alias default 10.15
+echo "Then execute: nvm install 10.15 && nvm alias default 10.15 && nvm use default"
