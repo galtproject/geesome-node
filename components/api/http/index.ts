db8ad40d745210d2ddb242c3c0e5bd092a6eeb6e
jonybang <Jonybange@gmail.com>
2019-05-04 02:28:31 +0200

    fixes

--- components/api/http/index.ts
+++ components/api/http/index.ts
@@ -81,25 +81,16 @@ module.exports = (geesomeApp: IGeesomeApp, port) => {
     });
 
     service.post('/v1/save-file', async (req, res) => {
+        req.pipe(req.busboy);
+        
         const body = {};
-
         req.busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
             body[fieldname] = val;
             console.log('field', body);
         });
-        req.busboy.on('file', function (fieldname, file, filename) {
-            body['file'] = {
-                name: filename,
-                stream: file
-            };
-            console.log('file', body);
+        req.busboy.on('file', async function (fieldname, file, filename) {
+            res.send(await geesomeApp.saveContent(filename, file, req.user.id, body['groupId']), 200);
         });
-        req.busboy.on('finish', async function() {
-            console.log('finish', body);
-            res.send(await geesomeApp.saveContent(body['file'].stream, body['file'].name, req.user.id, body['groupId']), 200);
-        });
-        
-        req.pipe(req.busboy);
     });
 
     service.get('/v1/get-content/:ipfsHash', async (req, res) => {
