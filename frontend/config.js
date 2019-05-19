b8625dea000cdd5c6803bbc3ab458dd7a3ea4a24
jonybang <Jonybange@gmail.com>
2019-05-19 13:54:27 +0200

    config

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-// serverBaseUrl: 'http://159.69.176.78:7711/',
-  serverBaseUrl: 'http://localhost:7711/'
+ serverBaseUrl: 'http://159.69.176.78:7711/',
+//  serverBaseUrl: 'http://localhost:7711/'
 };
