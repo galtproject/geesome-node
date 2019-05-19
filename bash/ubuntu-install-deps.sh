ecfa8b20a7471e338e7f11d4a2d8455343125063
jonybang <Jonybange@gmail.com>
2019-05-19 15:09:07 +0200

    full install

--- bash/ubuntu-install-deps.sh
+++ bash/ubuntu-install-deps.sh
@@ -1,4 +1,8 @@
 sudo apt-get update
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+
 sudo apt-get install python build-essential libssl-dev mysql-server -y
 mysql -uroot -proot -e "create database geesome_core; ALTER DATABASE geesome_core CHARACTER SET utf8 COLLATE utf8_general_ci;"
 mysql -uroot -proot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'"
