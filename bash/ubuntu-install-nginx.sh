ee584fd1cfc3f96a2930a94b6fead61732a06491
jonybang <Jonybange@gmail.com>
2019-05-19 17:11:26 +0200

    full install

--- bash/ubuntu-install-nginx.sh
+++ bash/ubuntu-install-nginx.sh
@@ -3,7 +3,7 @@ sudo apt-get install nginx software-properties-common -y
 sudo cp bash/uncert-nginx.conf /etc/nginx/sites-enabled/default
 
 read -p "Enter Your Domain: "  appDomain
-appDir=`pwd`
+appDir="$(pwd)/frontend/dist"
 
 read -p "Enter Your Email: "  userEmail
 
@@ -18,7 +18,7 @@ sudo apt-get update -y
 sudo apt-get install certbot python-certbot-nginx  -y
 
 sudo mkdir /var/www/$appDomain/
-sudo chmod -R www-data:www-data /var/www/
+sudo chown -R www-data:www-data /var/www/
 sudo certbot --webroot certonly -w=/var/www/$appDomain/ --email $userEmail --agree-tos -d $appDomain -n
 
 sudo cp bash/nginx.conf /etc/nginx/geesome.conf
@@ -28,4 +28,5 @@ sudo sed -i -e "s~\%app_dir\%~$appDir~g" /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
 
-(crontab -l 2>/dev/null; echo "0 0 * * * certbot renew --pre-hook 'service nginx stop' --post-hook 'service nginx start'") | crontab -
+(sudo crontab -l 2>/dev/null; echo "0 0 * * * certbot renew --pre-hook 'service nginx stop' --post-hook 'service nginx start'") | sudo crontab -
+(sudo crontab -l 2>/dev/null; echo "0 1 * * * dpkg --list 'linux-image*'|awk '{ if ($1=="ii") print $2}'|grep -v `uname -r` | xargs apt-get purge $1 -y") | sudo crontab -
