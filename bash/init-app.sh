fc6efdbd24b28b59689bfe224a3c22aaa03ac2d6
jonybang <Jonybange@gmail.com>
2019-05-19 15:41:22 +0200

    full install

--- bash/init-app.sh
+++ bash/init-app.sh
@@ -1,9 +1,10 @@
+npm i
+
 # frontend
 npm i -g yarn
 ( cd frontend && yarn && npm run dev-build )
 
 # backend
-npm i
 npm i -g pm2
 pm2 install typescript
 pm2 start ./index.ts --name "geesome-core"
