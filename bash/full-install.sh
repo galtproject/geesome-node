ad4a2c051571e64e3f3ffa594a294266410a9a84
jonybang <Jonybange@gmail.com>
2019-06-08 20:39:32 +0200

    check STORAGE_REPO in full install

--- bash/full-install.sh
+++ bash/full-install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! -z "$STORAGE_REPO" ] && [ ! -d "$STORAGE_REPO" ] 
+then
+    echo "STORAGE_REPO Directory $STORAGE_REPO DOES NOT exists." 
+    exit 1
+fi
+
 bash/ubuntu-install-deps.sh
 bash/init-app.sh
 bash/ubuntu-install-nginx.sh
