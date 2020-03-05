bf610202ea2a04cdb1448488905decf5a3debde3
jonybang <Jonybange@gmail.com>
2020-03-06 01:23:51 +0200

    fix-vulnerability

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -15,6 +15,99 @@ const UIThread = Object.assign({}, coreConfig({
   path: __dirname,
   disableObfuscator: true,
   // domainLock: ['localhost', '127.0.0.1'],
+  cacheGroups: {
+    vue: {
+      test: /[\\/]node_modules\/(vue.*)[\\/]/,
+      name: 'vue.js',
+      chunks: 'all',
+      enforce: true,
+      priority: -5
+    },
+    lodash: {
+      test: /[\\/]node_modules\/(lodash.*)[\\/]/,
+      name: 'lodash.js',
+      chunks: 'all',
+      enforce: true,
+      priority: -5
+    },
+    async: {
+      test: /[\\/]node_modules\/(async.*)[\\/]/,
+      name: 'async.js',
+      chunks: 'all',
+      enforce: true,
+      priority: -5
+    },
+    bluebird: {
+      test: /[\\/]node_modules\/(bluebird.*)[\\/]/,
+      name: 'bluebird.js',
+      chunks: 'all',
+      enforce: true,
+      priority: -5
+    },
+    bignumber: {
+      test: /[\\/]node_modules\/(bignumber.*)[\\/]/,
+      name: 'bignumber.js',
+      chunks: 'all',
+      enforce: true,
+      priority: -5
+    },
+    axios: {
+      test: /[\\/]node_modules\/(axios.*)[\\/]/,
+      name: 'axios.js',
+      chunks: 'all',
+      enforce: true,
+      priority: -5
+    },
+    sentry: {
+      test: /[\\/]node_modules\/(.*sentry.*)[\\/]/,
+      name: 'sentry.js',
+      chunks: 'all',
+      enforce: true,
+      priority: -5
+    },
+    moment: {
+      test: /[\\/]node_modules\/(moment.*)[\\/]/,
+      name: 'moment.js',
+      chunks: 'all',
+      enforce: true,
+      priority: -5
+    },
+    ethers: {
+      test: /[\\/]node_modules\/(ethers.*)[\\/]/,
+      name: 'ethers.js',
+      chunks: 'all',
+      enforce: true,
+      priority: -5
+    },
+    indexeddbshim: {
+      test: /[\\/]node_modules\/(indexeddbshim.*)[\\/]/,
+      name: 'indexeddbshim.js',
+      chunks: 'all',
+      enforce: true,
+      priority: -5
+    },
+    openpgp: {
+      test: /[\\/]node_modules\/(openpgp.*)[\\/]/,
+      name: 'openpgp.js',
+      chunks: 'all',
+      enforce: true,
+      priority: -5
+    },
+    'node-forge': {
+      test: /[\\/]node_modules\/(node-forge.*)[\\/]/,
+      name: 'node-forge.js',
+      chunks: 'all',
+      enforce: true,
+      priority: -5
+    },
+    galtproject: {
+      test: /[\\/]node_modules\/(.*galtproject.*)[\\/]/,
+      name: 'galtproject.js',
+      chunks: 'all',
+      enforce: true,
+      priority: -5
+    },
+  },
   copy: [
     {from: "./assets", to: "./assets"},
     {from: "./locale", to: "./locale"},
