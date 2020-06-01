9a57fc97d5b4d25b04ae2220fe5ad6fc17c979be
jonybang <Jonybange@gmail.com>
2020-06-02 01:34:29 +0200

    debug updateGroup

--- components/app/v1/index.ts
+++ components/app/v1/index.ts
@@ -772,7 +772,9 @@ class GeesomeApp implements IGeesomeApp {
   async updateGroup(userId, groupId, updateData) {
     await this.checkUserCan(userId, CorePermissionName.UserGroupManagement);
     groupId = await this.checkGroupId(groupId);
-    if (!(await this.canEditGroup(userId, groupId)) && !(await this.database.isHaveGroupPermission(userId, groupId, GroupPermissionName.EditGeneralData))) {
+    const groupPermission = await this.database.isHaveGroupPermission(userId, groupId, GroupPermissionName.EditGeneralData);
+    console.log('groupPermission', userId, groupId, groupPermission);
+    if (!(await this.canEditGroup(userId, groupId)) && !groupPermission) {
       throw new Error("not_permitted");
     }
     await this.database.updateGroup(groupId, updateData);
