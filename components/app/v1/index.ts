2b3790a0e326afa81cde1f8f55b2e7f27e7d7e01
jonybang <jonybange@gmail.com>
2021-07-28 00:32:52 +0300

    store secrets keys in data directory

--- components/app/v1/index.ts
+++ components/app/v1/index.ts
@@ -129,7 +129,7 @@ class GeesomeApp implements IGeesomeApp {
   }
 
   async getSecretKey(keyName, mode) {
-    const keyPath = `${__dirname}/${keyName}.key`;
+    const keyPath = `./data/${keyName}.key`;
     let secretKey;
     try {
       secretKey = fs.readFileSync(keyPath).toString();
