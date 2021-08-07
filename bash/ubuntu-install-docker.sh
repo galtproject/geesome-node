ff1421fb1910510def935d772df692cd7733cdd4
jonybang <jonybange@gmail.com>
2021-08-07 21:37:13 +0300

    add docker install

--- bash/ubuntu-install-docker.sh
+++ bash/ubuntu-install-docker.sh
@@ -3,6 +3,7 @@
 sudo apt-get update -y
 
 sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update -y
