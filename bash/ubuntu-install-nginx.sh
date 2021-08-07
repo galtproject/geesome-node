121c5c955f233294484ddcf85ac15598392b20b6
jonybang <jonybange@gmail.com>
2021-08-08 02:11:34 +0300

    update install nginx script

--- bash/ubuntu-install-nginx.sh
+++ bash/ubuntu-install-nginx.sh
@@ -7,18 +7,14 @@ sudo cp bash/uncert-nginx.conf /etc/nginx/sites-enabled/default
 [ -z "$DOMAIN" ] && read -p "Enter Your Domain: " DOMAIN
 [ -z "$EMAIL" ] && read -p "Enter Your Email: " EMAIL
 
-sudo sed -i -e "s~\%app_domain\%~$DOMAIN~g" /etc/nginx/sites-enabled/default
-sudo sed -i -e "s~\%app_dir\%~$appDir~g" /etc/nginx/sites-enabled/default
-
-sudo service nginx restart
-
 sudo add-apt-repository universe -y
 sudo add-apt-repository ppa:certbot/certbot -y
 sudo apt-get update -y
 sudo apt-get install certbot python3-certbot-nginx  -y
 
 DOMAIN_DIR="/var/www/$DOMAIN"
-DOMAIN_FRONTEND_DIR="$DOMAIN/frontend"
+DOMAIN_FRONTEND_DIR="$DOMAIN_DIR/frontend"
+DOMAIN_DIST_DIR="$DOMAIN_FRONTEND_DIR/dist"
 
 sudo mkdir -p $DOMAIN_DIR || :
 sudo chown -R www-data:www-data $DOMAIN_DIR
@@ -29,9 +25,12 @@ appFrontendDir="$rootDir/.docker-data/geesome_data/frontend"
 sudo chmod -R 755 $appFrontendDir
 sudo chown -R www-data:www-data $appFrontendDir
 
-echo "appFrontendDir: $appFrontendDir"
-echo "DOMAIN_FRONTEND_DIR: $DOMAIN_FRONTEND_DIR"
 ln -s $appFrontendDir $DOMAIN_FRONTEND_DIR
+
+sudo sed -i -e "s~\%app_domain\%~$DOMAIN~g" /etc/nginx/sites-enabled/default
+sudo sed -i -e "s~\%app_dir\%~$DOMAIN_DIST_DIR~g" /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
 
 certbotOutput=$( sudo certbot --webroot certonly -w=/var/www/$DOMAIN/ --email $EMAIL --agree-tos -d $DOMAIN -n 2>&1 )
 
