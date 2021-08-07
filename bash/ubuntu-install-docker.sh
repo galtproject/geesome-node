c44b0e73bbb4568555a8d029baa3738f87d80f89
jonybang <jonybange@gmail.com>
2021-08-07 21:40:26 +0300

    add docker install

--- bash/ubuntu-install-docker.sh
+++ bash/ubuntu-install-docker.sh
@@ -10,6 +10,10 @@ sudo apt-get update -y
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
 docker-compose build --no-cache && mkdir -p .docker-data
 
 sudo sed "s|/root/geesome-node|$PWD|g" < bash/geesome-docker.service > /etc/systemd/system/geesome-docker.service
