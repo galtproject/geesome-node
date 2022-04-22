372b0f95391d83f3a1b62fe3fce8a54ead26af8b
MicrowaveDev <jonybange@gmail.com>
2022-04-22 20:50:51 +0800

    fix docker-build

--- bash/docker-build.sh
+++ bash/docker-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose down --rmi all && docker-compose build --no-cache && mkdir -p .docker-data
+docker-compose rmi --f geesome-node_web && docker-compose build --no-cache && mkdir -p .docker-data
