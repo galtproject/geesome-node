2117032de441368793ad18584ddf5f7b18184ada
MicrowaveDev <jonybange@gmail.com>
2022-05-09 00:20:43 +0800

    update tests

--- test/databaseValues.test.ts
+++ test/databaseValues.test.ts
@@ -12,51 +12,45 @@ import {IGeesomeDatabaseModule} from "../app/modules/database/interface";
 const assert = require('assert');
 
 describe("databaseValues", function () {
-  const databaseConfig = {name: 'geesome_test', options: {logging: true, storage: 'database-test.sqlite'}};
+	const databaseConfig = {name: 'geesome_test', options: {logging: true, storage: 'database-test.sqlite'}};
 
-  let database: IGeesomeDatabaseModule;
+	let database: IGeesomeDatabaseModule;
 
-  const databases = ['sql'];
+	before(async () => {
+		database = await require('../app/modules/database')({config: {databaseConfig}});
+	});
 
-  databases.forEach((databaseService) => {
-    describe(databaseService + ' database', () => {
-      before(async () => {
-        database = await require('../app/modules/database')({config: {databaseConfig}});
-      });
+	after(async () => {
+		await database.flushDatabase();
+	});
 
-      after(async () => {
-        await database.flushDatabase();
-      });
+	it("should set and get values correctly", async () => {
+		assert.strictEqual(await database.getValue('test1'), null);
 
-      it("should set and get values correctly", async () => {
-        assert.strictEqual(await database.getValue('test1'), null);
+		await database.setValue('test1', 'test1Value');
 
-        await database.setValue('test1', 'test1Value');
+		assert.strictEqual(await database.getValue('test1'), 'test1Value');
 
-        assert.strictEqual(await database.getValue('test1'), 'test1Value');
+		await database.setValue('test1', 'test1ValueNew');
 
-        await database.setValue('test1', 'test1ValueNew');
+		assert.strictEqual(await database.getValue('test1'), 'test1ValueNew');
 
-        assert.strictEqual(await database.getValue('test1'), 'test1ValueNew');
+		assert.strictEqual(await database.getValue('test2'), null);
 
-        assert.strictEqual(await database.getValue('test2'), null);
+		await database.setValue('test2', 'test2Value');
+		assert.strictEqual(await database.getValue('test2'), 'test2Value');
 
-        await database.setValue('test2', 'test2Value');
-        assert.strictEqual(await database.getValue('test2'), 'test2Value');
+		assert.strictEqual(await database.getValue('test1'), 'test1ValueNew');
 
-        assert.strictEqual(await database.getValue('test1'), 'test1ValueNew');
+		await database.clearValue('test1');
 
-        await database.clearValue('test1');
+		assert.strictEqual(await database.getValue('test1'), null);
+		assert.strictEqual(await database.getValue('test2'), 'test2Value');
 
-        assert.strictEqual(await database.getValue('test1'), null);
-        assert.strictEqual(await database.getValue('test2'), 'test2Value');
+		await database.clearValue('test2');
 
-        await database.clearValue('test2');
+		assert.strictEqual(await database.getValue('test2'), null);
 
-        assert.strictEqual(await database.getValue('test2'), null);
-
-        await database.setValue('1 — копия.jpeg', '1 — копия.jpeg');
-      });
-    });
-  });
+		await database.setValue('1 — копия.jpeg', '1 — копия.jpeg');
+	});
 });
--- test/staticSiteGenerator.test.ts
+++ test/staticSiteGenerator.test.ts
@@ -12,11 +12,12 @@ import {
 	CorePermissionName,
 } from "../app/modules/database/interface";
 import IGeesomeStaticSiteManagerModule from "../app/modules/staticSiteGenerator/interface";
+
 const {getTitleAndDescription} = require('../app/modules/staticSiteGenerator/helpers');
 
 const assert = require('assert');
 
-describe("telegramClient", function () {
+describe("staticSiteGenerator", function () {
 	const databaseConfig = {
 		name: 'geesome_test', options: {
 			logging: () => {
@@ -28,57 +29,54 @@ describe("telegramClient", function () {
 
 	let app: IGeesomeApp, staticSiteGenerator: IGeesomeStaticSiteManagerModule;
 
-	const versions = ['v1'];//'ipfs-http-client'
+	beforeEach(async () => {
+		const appConfig = require('../app/config');
+		appConfig.storageConfig.implementation = 'js-ipfs';
+		appConfig.storageConfig.jsNode.repo = '.jsipfs-test';
+		appConfig.storageConfig.jsNode.pass = 'test test test test test test test test test test';
+		appConfig.storageConfig.jsNode.config = {
+			Addresses: {
+				Swarm: [
+					"/ip4/0.0.0.0/tcp/40002",
+					"/ip4/127.0.0.1/tcp/40003/ws",
+					"/dns4/wrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star"
+				]
+			}
+		};
 
-	versions.forEach((appVersion) => {
-		describe('app ' + appVersion, () => {
-			beforeEach(async () => {
-				const appConfig = require('../app/config');
-				appConfig.storageConfig.implementation = 'js-ipfs';
-				appConfig.storageConfig.jsNode.repo = '.jsipfs-test';
-				appConfig.storageConfig.jsNode.pass = 'test test test test test test test test test test';
-				appConfig.storageConfig.jsNode.config = {
-					Addresses: {
-						Swarm: [
-							"/ip4/0.0.0.0/tcp/40002",
-							"/ip4/127.0.0.1/tcp/40003/ws",
-							"/dns4/wrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star"
-						]
-					}
-				};
+		try {
+			app = await require('../app')({databaseConfig, storageConfig: appConfig.storageConfig, port: 7771});
 
-				try {
-					app = await require('../app')({databaseConfig, storageConfig: appConfig.storageConfig, port: 7771});
-
-					await app.setup({email: 'admin@admin.com', name: 'admin', password: 'admin'});
-					const testUser = await app.registerUser({
-						email: 'user@user.com',
-						name: 'user',
-						password: 'user',
-						permissions: [CorePermissionName.UserAll]
-					});
-					await app.ms.group.createGroup(testUser.id, {
-						name: 'test',
-						title: 'Test'
-					});
-					staticSiteGenerator = app.ms['staticSiteGenerator'];
-				} catch (e) {
-					console.error('error', e);
-					assert.equal(true, false);
-				}
+			await app.setup({email: 'admin@admin.com', name: 'admin', password: 'admin'});
+			const testUser = await app.registerUser({
+				email: 'user@user.com',
+				name: 'user',
+				password: 'user',
+				permissions: [CorePermissionName.UserAll]
 			});
-
-			afterEach(async () => {
-				await app.ms.database.flushDatabase();
-				await app.stop();
+			await app.ms.group.createGroup(testUser.id, {
+				name: 'test',
+				title: 'Test'
 			});
+			staticSiteGenerator = app.ms['staticSiteGenerator'];
+		} catch (e) {
+			console.error('error', e);
+			assert.equal(true, false);
+		}
+	});
 
-			it('webpage message should import properly', async () => {
-				const text = 'Кто плюсист?<br><a href="https://en.wikipedia.org/wiki/C%2B%2B20">https://en.wikipedia.org/wiki/C%2B%2B20</a><br><br><i>Language<br>concepts[6], with terse syntax.[7]<br>modules[8]<br><br>Library<br>ranges (The One Ranges Proposal)[35]</i>';
-				const {title, description} = getTitleAndDescription([{view: 'contents', text}], {titleLength: 200, descriptionLength: 200})
-				console.log('title', title, 'description', description);
-				assert.equal(title, 'Кто плюсист?<br/><a href="https://en.wikipedia.org/wiki/C%2B%2B20">https://en.wikipedia.org/wiki/C%2B%2B20</a><br/><br/><i>Language<br/>concepts[6], with terse syntax.[7]<br/>modules[8]<br/><br/>Library<br/>r...</i>');
-			});
-		});
+	afterEach(async () => {
+		await app.ms.database.flushDatabase();
+		await app.stop();
+	});
+
+	it('webpage message should import properly', async () => {
+		const text = 'Кто плюсист?<br><a href="https://en.wikipedia.org/wiki/C%2B%2B20">https://en.wikipedia.org/wiki/C%2B%2B20</a><br><br><i>Language<br>concepts[6], with terse syntax.[7]<br>modules[8]<br><br>Library<br>ranges (The One Ranges Proposal)[35]</i>';
+		const {title, description} = getTitleAndDescription([{view: 'contents', text}], {
+			titleLength: 200,
+			descriptionLength: 200
+		})
+		console.log('title', title, 'description', description);
+		assert.equal(title, 'Кто плюсист?<br/><a href="https://en.wikipedia.org/wiki/C%2B%2B20">https://en.wikipedia.org/wiki/C%2B%2B20</a><br/><br/><i>Language<br/>concepts[6], with terse syntax.[7]<br/>modules[8]<br/><br/>Library<br/>r...</i>');
 	});
 });
