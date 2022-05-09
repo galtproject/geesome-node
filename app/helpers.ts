1196abe878f21107bb19337eafead4852f72211d
MicrowaveDev <jonybange@gmail.com>
2022-05-09 21:07:02 +0800

    move password functions to helpers

--- app/helpers.ts
+++ app/helpers.ts
@@ -1,4 +1,6 @@
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 module.exports = {
 	validateEmail(email) {
@@ -9,6 +11,25 @@ module.exports = {
 		return /^\w+([\.-]?\w)+$/.test(username) && username.length <= 42;
 	},
 
+	async hashPassword(password) {
+		return new Promise((resolve, reject) => {
+			if (!password) {
+				return resolve(null);
+			}
+			bcrypt.hash(password, saltRounds, async (err, passwordHash) => err ? reject(err) : resolve(passwordHash));
+		})
+	},
+
+	async comparePasswordWithHash(password, passwordHash) {
+		if (!password || !passwordHash) {
+			return false;
+		}
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, passwordHash, (err, result) => err ? reject(err) : resolve(!!result));
+		});
+	},
+
+
 	log(){
 		const logArgs = _.map(arguments, (arg) => arg);
 
@@ -16,5 +37,5 @@ module.exports = {
 		logArgs.splice(0, 0, dateTimeStr);
 
 		console.log.apply(console, logArgs);
-	}
+	},
 }
