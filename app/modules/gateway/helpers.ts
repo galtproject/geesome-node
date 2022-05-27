4f952c17292b2955bcde0cb28a1c2f7ca7ff509a
MicrowaveDev <jonybange@gmail.com>
2022-05-27 21:16:59 +0800

    fix gateway dig

--- app/modules/gateway/helpers.ts
+++ app/modules/gateway/helpers.ts
@@ -2,8 +2,7 @@ const _ = require('lodash');
 const childProcess = require("child_process");
 
 module.exports = {
-	async getDnsLinkPathFromHost(req) {
-		const {host} = req.headers;
+	async getDnsLinkPathFromHost(host) {
 		return new Promise((resolve, reject) => {
 			childProcess.exec(`dig -t txt ${host.split(':')[0]} +short`, (e, output) => e ? reject(e) : resolve(_.trim(output, '"\n').split('=')[1]));
 		}) as Promise<string>;
