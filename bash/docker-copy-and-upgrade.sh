d8fc0b5205035ad2e3b780bf229a6c512532bd69
MicrowaveDev <jonybange@gmail.com>
2024-11-10 22:40:42 +0000

    remove unused bash scripts

--- bash/docker-copy-and-upgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git checkout -- .
-git pull
-
-sudo chmod +x bash/*.sh && ./bash/docker-copy-app.sh
-
-docker restart geesome
--- bash/ubuntu-install-go-ipfs.sh
+++
@@ -1,7 +0,0 @@
-#wget https://dist.ipfs.io/go-ipfs/v0.4.20/go-ipfs_v0.4.20_linux-amd64.tar.gz
-#tar xvfz go-ipfs_v0.4.20_linux-amd64.tar.gz
-#sudo mv go-ipfs/ipfs /usr/local/bin/ipfs
-#rm go-ipfs_v0.4.20_linux-amd64.tar.gz && rm -R ./go-ipfs
-#IPFS_PATH=~/.ipfs ipfs init --profile server
-#cp bash/go-ipfs.service /lib/systemd/system/go-ipfs.service
-#systemctl daemon-reload && systemctl start go-ipfs
