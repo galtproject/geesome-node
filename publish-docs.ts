ce59bf3c94eab76df6c4f1a8708532bb67dbd7a1
MicrowaveDev <jonybange@gmail.com>
2024-11-10 23:48:24 +0000

    fix publish docs

--- publish-docs.ts
+++ publish-docs.ts
@@ -13,17 +13,19 @@ import {IGeesomeApp} from "./app/interface";
 import helpers from "./app/helpers";
 
 export default (app: IGeesomeApp) => {
-  if (!fs.existsSync(helpers.getCurDir() + '/node_modules/.bin/apidoc')) {
+  const apiDocPath = helpers.getCurDir() + '/../node_modules/.bin/apidoc';
+  console.log('apiDocPath', apiDocPath);
+  if (!fs.existsSync(apiDocPath)) {
     return console.warn("Script for generating docs not found :(");
   }
   //TODO: include modules api
-  const child = spawn(helpers.getCurDir() + '/node_modules/.bin/apidoc', ['-i', 'app/modules/api', '-o', 'docs/',  '-t', 'node_modules/geesome-apidoc-template/template']);
+  const child = spawn(apiDocPath, ['-i', 'app/modules/api', '-o', 'docs/',  '-t', 'node_modules/geesome-apidoc-template/template']);
 
   child.on('close', async (code) => {
     if (code !== 0) {
       return console.warn("Docs publishing failed :(");
     }
-    const result = await app.ms.storage.saveDirectory(helpers.getCurDir() + '/docs');
+    const result = await app.ms.storage.saveDirectory(helpers.getCurDir() + '/../docs');
 
     console.log('Docs IPFS:', result.id);
 
