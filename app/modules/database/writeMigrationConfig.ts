68afb38ebf14a58adbfd620406c6c6963521dc37
MicrowaveDev <jonybange@gmail.com>
2024-11-11 00:59:28 +0000

    refactor writeMigrationConfig and update geesome-libs

--- app/modules/database/writeMigrationConfig.ts
+++ app/modules/database/writeMigrationConfig.ts
@@ -1,26 +1,3 @@
-/*
- * Copyright ©️ 2018-2020 Galt•Project Society Construction and Terraforming Company
- * (Founded by [Nikolai Popeka](https://github.com/npopeka)
- *
- * Copyright ©️ 2018-2020 Galt•Core Blockchain Company
- * (Founded by [Nikolai Popeka](https://github.com/npopeka) by
- * [Basic Agreement](ipfs/QmaCiXUmSrP16Gz8Jdzq6AJESY1EAANmmwha15uR3c1bsS)).
- */
+import writeMigrationConfig from "../../writeMigrationConfig";
 
-import fs from "fs";
-import helpers from "../../helpers.js";
-import config from'./config.js';
-
-const modulePath = `${helpers.getCurDir()}/modules/database/config/`;
-if (!fs.existsSync(modulePath)) {
-  fs.mkdirSync(modulePath);
-}
-fs.writeFileSync(`${modulePath}config.json`, JSON.stringify({
-  production: {
-    database: config.database,
-    username: config.username,
-    password: config.password,
-    host: config.host,
-    dialect: config.dialect
-  }
-}));
+writeMigrationConfig('database');
