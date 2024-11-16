9e467869cd59c566a8a85a5841d484e648fae991
MicrowaveDev <jonybange@gmail.com>
2024-11-16 15:29:26 +0000

    use local scripts in static sites

---
+++ app/modules/staticSiteGenerator/site/vendorAssets.js
@@ -0,0 +1,6 @@
+export default {
+    "vue.js": "node_modules/vue/dist/vue.esm-browser.js",
+    "vue-router.js": "node_modules/vue-router/dist/vue-router.esm-browser.js",
+    "vue-devtools-api": "node_modules/@vue/devtools-api/lib/esm/",
+    "es-module-shims.js": "node_modules/es-module-shims/dist/es-module-shims.js"
+}
